f30a95edc2f552b553f4ebe2bfeedb7406698ff0
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-02-26 12:51:23 -0800

    Basic explanation
    Gave the user a basic explanation on what the program does and greeted them

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -4,9 +4,16 @@ Program that makes a dinner guest list and prints out invitations
 """
 
 # Greet the user
-
+print(f"\nHi user!")
+print("Wanting to hold out a dinner date at your house or anywhere?")
+print("But don't know how to invite the guests?")
+print(f"Well then, you are at the right spot!\n")
+print("Welcome to the dinner guest list prgram!")
+name = input(f"To begin, what is your name?  ").strip().title()
+print(f"Sup {name}! Let's make a good dinner guest list together!")
 
 # Ask the user how many guests they want to invite
+
 
 
 # Show them the list
@@ -23,6 +30,9 @@ Program that makes a dinner guest list and prints out invitations
 
 
 # Print out the invitations
+
+
+
 
 
 # Goodbye
