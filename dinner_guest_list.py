76919dd4de17ead29d07c10dd680e3e835e60aa1
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-02-26 12:44:17 -0800

    Pseudocode
    Added pseudocode to get an idea on what I am going to code

---
+++ dinner_guest_list.py
@@ -0,0 +1,28 @@
+
+"""
+Program that makes a dinner guest list and prints out invitations
+"""
+
+# Greet the user
+
+
+# Ask the user how many guests they want to invite
+
+
+# Show them the list
+
+
+
+
+# Ask the user if they want to replace someone
+
+
+
+# Final list
+
+
+
+# Print out the invitations
+
+
+# Goodbye
