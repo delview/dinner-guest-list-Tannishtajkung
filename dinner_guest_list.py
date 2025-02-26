c61bc8f0e31082966cb46ea3f729c0b1a2cf0fad
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-02-26 13:04:18 -0800

    Adding guests
    Did the adding guests part, all worked out well.

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -2,6 +2,28 @@
 """
 Program that makes a dinner guest list and prints out invitations
 """
+guest_list = []
+
+def add():
+    add_num = 1
+    while True:
+        try:
+            add_times = int(input("How many people do you want to invite?:  "))
+            if add_times <= 0:
+                print(f"You need atleast 1 person in your list.\n")
+                continue
+            break
+        except ValueError:
+            print(f"Invalid! Enter a number.\n")
+            continue
+            
+
+    for x in range(add_times):
+        guest_list.append(input(f"Guest {add_num}:  ").strip().title())
+        add_num = add_num + 1
+    
+    return
+
 
 # Greet the user
 print(f"\nHi user!")
@@ -9,11 +31,11 @@ print("Wanting to hold out a dinner date at your house or anywhere?")
 print("But don't know how to invite the guests?")
 print(f"Well then, you are at the right spot!\n")
 print("Welcome to the dinner guest list prgram!")
-name = input(f"To begin, what is your name?  ").strip().title()
-print(f"Sup {name}! Let's make a good dinner guest list together!")
+name = input(f"To begin, what is your name?  ").strip().capitalize()
+print(f"Sup {name}! Let's make a good dinner guest list together!\n")
 
 # Ask the user how many guests they want to invite
-
+add()
 
 
 # Show them the list
