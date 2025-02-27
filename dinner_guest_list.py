5730d1dad673fd11ff67fed750618de1e3d5370a
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-02-27 09:18:56 -0800

    Major changes, new function
    Made some major changes, like adding a new function called remove, fixed replace function as well, and made the code neat and readable.

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -4,7 +4,7 @@ Program that makes a dinner guest list and prints out invitations
 """
 guest_list = []
 
-def add():
+def add(): # Function that adds guests to the list
     add_num = 1
     while True:
         try:
@@ -23,14 +23,42 @@ def add():
 
     print(f"\nHere's your guest list:")
     for guests in guest_list:
-        print(guests)
+        print(guests) # Prints out the guest list
 
     print(f"\nThe number of people, you invited-: ")
-    print(len(guest_list))
+    print(len(guest_list)) # Prints out the number of people in the guest list
     print("")
     return
 
-def replace():
+
+def remove(): # Function that removes guests from the list
+    remove_num = 1
+    while True:
+        try:
+            remove_times = int(input("How many people do you want to remove?:  "))
+            if remove_times <= 0:
+                print(f"You need atleast 1 person in your list.\n")
+                continue
+            break
+        except ValueError:
+            print(f"Invalid! Enter a number.\n")
+            continue   
+
+    for x in range(remove_times):
+        guest_list.remove(input(f"Guest {remove_num}:  ").strip().title())
+        remove_num = remove_num + 1
+
+    print(f"\nHere's your guest list:")
+    for guests in guest_list:
+        print(guests) # Prints out the guest list
+
+    print(f"\nThe number of people, you invited-: ")
+    print(len(guest_list)) # Prints out the number of people in the guest list
+    print("")
+    return
+
+
+def replace(): # Function that replaces a guest in the list
     replace_num = 1
     while True:
         try:
@@ -38,18 +66,22 @@ def replace():
             if replace_times <= 0:
                 print("If you don't want to replace anyone in your list, then let's go back.")
                 return
-            else:
-                for x in range(replace_times):
-                    guest_list.replace(input(f"Replace {replace_num}:").strip().title())
-                    replace_num = replace_num + 1
-
+            break
         except ValueError:
             print(f"Invalid! Enter a number\n")
-
+            continue
+        
+    for x in range(replace_times):
+        guest_list.replace(input(f"Replace {replace_num}:").strip().title())
+        replace_num = replace_num + 1
         
     print(f"\nHere's your guest list:")
     for guests in guest_list:
-        print(guests)
+        print(guests) # Prints out the guest list
+
+    print(f"\nThe number of people, you invited-: ")
+    print(len(guest_list)) # Prints out the number of people in the guest list
+    print("")
         
 
 # Greet the user
@@ -61,42 +93,59 @@ print("Welcome to the dinner guest list prgram!")
 name = input(f"To begin, what is your name?  ").strip().capitalize()
 print(f"Sup {name}! Let's make a good dinner guest list together!\n")
 
-# Ask the user how many guests they want to invite
+# Bring out the add guests function
 add()
 
-# Show them the list
+# Ask the user if they want to add someone else in the list 
+while True: # Loop if choice_add is yes
+    while True: # Input problem
+        choice_add = input("Forgot someone? Do you want to add anyone else in the list? [y/n]:  ").strip().lower()
+        if choice_add in ['y', 'n']:
+            break
+        print(f"Invalid! Enter either [y]es or [n]o.\n")
+        continue
 
+    if choice_add == "y":
+        add()
 
-
-# Ask the user if they want to add someone else in the list too
-while True:
-    choice_add = input("Forgot someone? Do you want to add anyone in the list? [y/n]:  ").strip().lower()
-    if choice_add in ['y', 'n']:
+    if choice_add == "n":
         break
-    print(f"Invalid! Enter either [y]es or [n]o.\n")
-    continue
 
-if choice_add == "y":
-    add()
+# Ask the user if they want to remove anyone from the list
+while True: # Loop if choice_remove is yes
+    while True: # Input problem
+        choice_remove = input("Forgot someone? Do you want to add anyone else in the list? [y/n]:  ").strip().lower()
+        if choice_remove in ['y', 'n']:
+            break
+        print(f"Invalid! Enter either [y]es or [n]o.\n")
+        continue
 
+    if choice_remove == "y":
+        remove()
 
-# Ask the user if they want to replace someone
-while True:
-    choice_replace = input("Do you want to replace anyone in the list? [y/n]:  ").strip().lower()
-    if choice_replace in ['y', 'n']:
+    if choice_remove == "n":
         break
-    print(f"Invalid! Enter either [y]es or [n]o.\n")
-    continue
+    
+# Ask the user if they want to replace someone from the list
+while True: # Loop if choice_replace is yes
+    while True: # Input problem
+        choice_replace = input("Do you want to replace anyone in the list? [y/n]:  ").strip().lower()
+        if choice_replace in ['y', 'n']:
+            break
+        print(f"Invalid! Enter either [y]es or [n]o.\n")
+        continue
 
-if choice_replace == "y":
-    replace()
+    if choice_replace == "y":
+        replace()
 
-elif choice_replace == "n": # Final list
-    print("Here's your final guest list:")
-    for guests in guest_list:
-        print(guests)
-    print(f"\nThe number of people, you invited-: ")
-    print(len(guest_list))
+    elif choice_replace == "n": # Final list
+        print(f"\nHere's your final guest list:")
+        for guests in guest_list:
+            print(guests)
+        print(f"\nThe number of people, you invited-: ")
+        print(len(guest_list))
+        print("")
+        break
 
 # Print out the invitations
 
@@ -105,4 +154,4 @@ elif choice_replace == "n": # Final list
 
 
 # Goodbye
-print("Thanks for using the program, hope your dinner party goes well! ")
+print(f"Thanks for using the program, hope your dinner party goes well!\n")
